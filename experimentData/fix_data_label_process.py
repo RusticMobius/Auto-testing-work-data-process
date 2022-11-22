14b1656bf4adca5342a8804b667f23afc8b98d23
莫库什勒 <oncwnuFkfY-eKXufrATCAUJn1hbM@git.weixin.qq.com>
2022-11-22 16:58:03 +0800

    feat:完成正报告贴标签脚本

--- experimentData/fix_data_label_process.py
+++ experimentData/fix_data_label_process.py
@@ -2,6 +2,8 @@ import csv
 
 def label_process(file_path):
 
+
+
   with open(file_path) as f:
     reader = csv.reader(f)
     label_list = ["close","open","unknown"]
@@ -13,19 +15,27 @@ def label_process(file_path):
       project_name = spot_commit_info[1]
       spot_commit_id = spot_commit_info[2]
       spot_target_file = spot_commit_info[3].split("/")[-1]
-      spot_commit_start_line = commits_info[-2]
-      spot_commit_end_line = commits_info[-1]
+      spot_commit_start_line = spot_commit_info[-2]
+      spot_commit_end_line = spot_commit_info[-1]
 
       fix_commit_id = fix_commit_info[1]
       fix_target_file = fix_commit_info[2].split("/")[-1]
 
-      file_compare = spot_target_file + "(" + "=>" + fix_target_file
+      file_compare = spot_target_file + "(" + spot_commit_start_line + ":" + spot_commit_end_line + ")" + " => " + fix_target_file
       git_compare_url = "https://github.com/apache/" + project_name + "/compare/" + spot_commit_id + ".." + fix_commit_id
 
       print(file_compare)
       print(git_compare_url)
-      label_index = int(input("close: 1   open: 2   unknown: 3")) - 1
+      label_index = int(input("close: 1   open: 2   unknown: 3\nenter label: ")) - 1
       label = label_list[label_index]
+      print("------------------------------------------")
+
+      write_file_path = "data-phases-1/fixed-project-summary/fixed-label-dir/" + project_name + ".csv"
+      with open(write_file_path,"a+",encoding='utf8',newline='') as write_file:
+        writer = csv.writer(write_file)
+        line = [label] + spot_commit_info
+        writer.writerow(line)
+
 
 
 
